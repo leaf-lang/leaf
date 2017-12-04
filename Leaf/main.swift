ed08f6ca90ea611f39d8549a9779380bae7efc4a
Peter <petertheanimator@gmail.com>
2017-12-04 21:43:52 +0000

    Initial Commit

---
+++ Leaf/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Leaf
+//
+//  Created by Peter Entwistle on 04/12/2017.
+//  Copyright © 2017 Peter Entwistle. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
